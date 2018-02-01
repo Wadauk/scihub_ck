2dc3b2f07aa05bcd2ca77c60b9cb8a687c2bb230
Wadauk <2336221276@qq.com>
2018-02-01 19:20:25 +0800

    upload the auto.sh

---
+++ auto.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# by Wadauk @ https://weibo.com/u/1657589804
+
+# check all the domains
+perl scihub_ck
+
+# remove the other domains
+cd web
+yes \
+  | rm `ls -l \
+  | grep -v 26860 \
+  | grep -v total \
+  | sed -e "s/.* //"`
+
+# check the speed
+for domain in `ls | grep -v .txt`; \
+  do \
+  echo $domain >> dm.txt; \
+  curl -o /dev/null -s -w '%{time_total}\n' "http://sci-hub.$domain" >> sp.txt; \
+done;
+
+# combine the result in one file
+paste dm.txt sp.txt > data.txt
+
+# sort the domains by speed
+sort -k2n data.txt > data2.txt
+
+# creat web by data
+cd ..
+perl /home/pi/data2index.pl web/data2.txt > index.html
+
+# remove temp files
+yes | rm web/*
+
+# upload to github
+git status 
+git add index.html 
+git status 
+git commit -m "this is my commit info note."
+git push origin 
