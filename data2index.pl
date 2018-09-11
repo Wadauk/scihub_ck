07de3ef0a30071ee77681f7be8a7d8b8d96d984f
Wadauk <2336221276@qq.com>
2018-09-11 11:58:50 +0800

    mu is blocked

--- data2index.pl
+++ data2index.pl
@@ -40,7 +40,7 @@ var _hmt = _hmt || [];
 <div class="ui container">
 <div class="ui stackable grid">
 <div class="fifteen wide column">
-<iframe src="https://ghbtns.com/github-btn.html?user=Wadauk&repo=scihub_ck&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe><p>Tips: ooo and nu are blocked. is is re-opened but does not work well. Recommend: tw and mu.</p>
+<iframe src="https://ghbtns.com/github-btn.html?user=Wadauk&repo=scihub_ck&type=star&count=true&size=large" frameborder="0" scrolling="0" width="160px" height="30px"></iframe><p>Tips: ooo/nu/mu are blocked. is is re-opened but does not work well. Recommend: tw and se.</p>
 <div class="ui top attached segment">
 <div class="ui middle aligned two column grid">
 <div class="column">
